db2c45eb0fcaedac8cff7903125d345bfec3211a
Lizard Rush <151949502+LizardRush@users.noreply.github.com>
2026-03-13 18:52:13 -0400

    Create BobGUI.lua

---
+++ BobGUI.lua
@@ -0,0 +1,103 @@
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local Window = Rayfield:CreateWindow({
+	Name = "LRush's Bob fighter",
+	LoadingTitle = "LRush's Bob fighter",
+	LoadingSubtitle = "by LRush",
+	ConfigurationSaving = {
+		Enabled = false
+	}
+})
+
+local HomeTab = Window:CreateTab("Home", 4483362458)
+
+local DamageBob = false
+local AutoTycoon = false
+local EverythingActive = false
+local AttackBobMinions = false
+
+HomeTab:CreateToggle({
+	Name = "Everything Active",
+	CurrentValue = false,
+	Callback = function(Value)
+		EverythingActive = Value
+		print("Everything Active:", Value)
+	end
+})
+
+HomeTab:CreateToggle({
+	Name = "Damage Bob",
+	CurrentValue = false,
+	Callback = function(Value)
+		DamageBob = Value
+		print("Damage Bob:", Value)
+	end
+})
+
+HomeTab:CreateToggle({
+	Name = "Auto Tycoon",
+	CurrentValue = false,
+	Callback = function(Value)
+		AutoTycoon = Value
+		print("Auto Tycoon:", Value)
+	end
+})
+
+HomeTab:CreateToggle({
+	Name = "Kill Bob Minions",
+	CurrentValue = false,
+	Callback = function(Value)
+		AutoTycoon = Value
+		print("Kill Bob Minions:", Value)
+	end
+})
+
+local function findTycoon()
+	for _, obj in ipairs(workspace:GetDescendants()) do
+		if obj.Name:sub(1,7) == "ÅTycoon" then
+			return obj
+		end
+	end
+end
+
+game["Run Service"].Heartbeat:Connect(function()
+	if not EverythingActive then return end
+	if DamageBob then
+		local Event = game:GetService("Workspace").bobBoss.DamageEvent
+		Event:FireServer()
+	end
+	if AutoTycoon then
+		local Event = findTycoon().Click.ClickDetector
+		fireclickdetector(Event)
+	end
+end)
+
+while true do
+	if AttackBobMinions and EverythingActive then
+		for _, obj in ipairs(workspace:GetDescendants()) do
+			if obj.Name == "BobMinion" then
+				local character = player.Character
+				if character and character:FindFirstChild("HumanoidRootPart") then
+					local root = character.HumanoidRootPart
+
+					if obj:IsA("BasePart") then
+						root.CFrame = obj.CFrame + Vector3.new(0, 3, 0)
+					elseif obj:IsA("Model") and obj:FindFirstChild("HumanoidRootPart") then
+						root.CFrame = obj.HumanoidRootPart.CFrame + Vector3.new(0, 3, 0)
+					end
+
+					local tool = character:FindFirstChildOfClass("Tool")
+					if tool then
+						tool:Activate()
+					end
+
+					task.wait(0.5)
+				end
+			end
+		end
+	end
+	task.wait()
+end
