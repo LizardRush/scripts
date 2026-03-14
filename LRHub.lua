6d15f0786a70cf716118d7636fc4ef769ca3f7a7
Lizard Rush <151949502+LizardRush@users.noreply.github.com>
2026-03-14 15:30:57 -0400

    Update LRHub.lua

--- LRHub.lua
+++ LRHub.lua
@@ -1,5 +1,7 @@
 local placeId = game.PlaceId
 local url = "https://raw.githubusercontent.com/LizardRush/scripts/refs/heads/main/" .. placeId
+local Fallbackurl = "https://raw.githubusercontent.com/LizardRush/scripts/refs/heads/main/Fallback.lue"
+
 
 local success, response = pcall(function()
     return game:HttpGet(url)
@@ -8,5 +10,5 @@ end)
 if success and response and #response > 0 then
     loadstring(response)()
 else
-    warn("Invalid place. No script found for PlaceId:", placeId)
+    loadstring(game:HttpGet(Fallbackurl))()
 end
