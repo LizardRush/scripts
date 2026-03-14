0cb4c43b1e9be2b515d0a3aa45c7a7d2ace4da6c
Lizard Rush <151949502+LizardRush@users.noreply.github.com>
2026-03-14 15:03:04 -0400

    Update LRHub.lua

--- LRHub.lua
+++ LRHub.lua
@@ -1,1 +1,12 @@
+local placeId = game.PlaceId
+local url = "https://raw.githubusercontent.com/LizardRush/scripts/refs/heads/main/" .. placeId
 
+local success, response = pcall(function()
+    return game:HttpGet(url)
+end)
+
+if success and response and #response > 0 then
+    loadstring(response)()
+else
+    warn("Invalid place. No script found for PlaceId:", placeId)
+end
